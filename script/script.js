b7498a7775bfa47dd061e3642d0da0182e0b7824
seebiprogrammer <118176737+seebiprogrammer@users.noreply.github.com>
2023-05-25 22:13:52 +0500

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,16 +1,13 @@
-const navBar = document.querySelector(".nav-bar");
-const barsIcon = document.querySelector(".menu-icon .bars");
-const timesIcon = document.querySelector(".menu-icon .times");
-const menuIcon = document.querySelector(".menu-icon");
 const topHead = document.querySelector(".top-head");
+const menuIcon = document.querySelector(".menu-icon");
 
 menuIcon.addEventListener("click", () => {
+  topHead.classList.toggle("active");
   menuIcon.classList.toggle("fa-times");
-  navBar.classList.toggle("active");
 });
 
 window.onscroll = () => {
-  navBar.classList.remove("active");
+  topHead.classList.remove("active");
   menuIcon.classList.remove("fa-times");
   menuIcon.classList.add("fa-bars");
 };
